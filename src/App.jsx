cd7914f3999e715a2828424e6dce92e81d540251
W3str <oswe01@gmail.com>
2023-11-27 17:06:11 +0100

    HomeView start

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
 // Root component of the application
+import React from 'react';
+import HomeView from './views/HomeView';
+
+function App() {
+    return (
+        <div className="App">
+        <HomeView />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePresenter from '../presenters/HomePresenter';
+
+function HomeView() {
+    return (
+        <div className="HomeView">
+        <HomePresenter />
+        </div>
+    );
+}
+
+export default HomeView;
